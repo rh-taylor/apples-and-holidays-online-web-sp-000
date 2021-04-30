17fd126d954e495b2145be29c012dc0626dc30ea
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-30 18:57:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
     binding.pry
-    puts "  #{holiday_name.to_s.split("_").collect { |split_holiday_name|.capitalize }.join(", ")}"
+    puts "  #{holiday_name.to_s.split("_").collect { |split_holiday_name| split_holiday_name.capitalize }.join(", ")}"
     end
   end    
 end
