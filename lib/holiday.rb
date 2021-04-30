5a9635dc162277e513993f9c1d46e23c5e402a65
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-30 19:21:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.collect do |holiday_name, supplies|
       supplies.include?("BBQ") ? holiday_name : nil
     end
-  end
+  end.flatten.compact
 end
 
 
