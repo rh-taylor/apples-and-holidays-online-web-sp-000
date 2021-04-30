9ca6d2fdfc4f2c2c004c1474378f5f18600c0b86
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-30 19:04:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,6 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
   return holiday_hash[:winter].values.flatten
 end
 
